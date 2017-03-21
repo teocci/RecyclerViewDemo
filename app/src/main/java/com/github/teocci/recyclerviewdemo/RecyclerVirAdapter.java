d804fc122ead5d1977300eeea6e24f3aa0f4c70c
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-03-21 10:52:26 +0900

    first commit

---
+++ app/src/main/java/com/github/teocci/recyclerviewdemo/RecyclerVirAdapter.java
@@ -0,0 +1,64 @@
+package com.github.teocci.recyclerviewdemo;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by teocci on 3/13/17.
+ */
+
+public class RecyclerVirAdapter extends RecyclerView.Adapter<RecyclerVirAdapter.BaseViewHolder>
+{
+    private String[] dataSet;
+
+    // Provide a suitable constructor (depends on the kind of dataSet)
+    public RecyclerVirAdapter(String[] dataSet)
+    {
+        this.dataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        BaseViewHolder viewHolder = new BaseViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder holder, int position)
+    {
+        holder.mTextView.setText(dataSet[position]);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return dataSet.length;
+    }
+
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class BaseViewHolder extends RecyclerView.ViewHolder
+    {
+        public CardView mCardView;
+        public TextView mTextView;
+
+        public BaseViewHolder(View v)
+        {
+            super(v);
+
+            mCardView = (CardView) v.findViewById(R.id.card_view);
+            mTextView = (TextView) v.findViewById(R.id.tv_text);
+        }
+    }
+}
---
+++ app/src/main/java/com/github/teocci/recyclerviewdemo/ui/BlankFragment.java
@@ -0,0 +1,61 @@
+package com.github.teocci.recyclerviewdemo.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.teocci.recyclerviewdemo.R;
+import com.github.teocci.recyclerviewdemo.RecyclerVirAdapter;
+
+/**
+ * Created by teocci on 3/13/17.
+ */
+
+public class BlankFragment extends Fragment
+{
+    public BlankFragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerVirAdapter adapter = new RecyclerVirAdapter(new String[]{
+                "test one",
+                "test two",
+                "test three",
+                "test four",
+                "test five",
+                "test six",
+                "test seven",
+                "test eight",
+                "test nine",
+                "test ten",
+                "test eleven",
+                "test twelve"
+        });
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        return rootView;
+    }
+}
